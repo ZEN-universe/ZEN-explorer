540a81989b47cc7cb03f38496da0f9b3aaf4d452
Vinzenz <muserv@ethz.ch>
2023-10-16 17:28:01 +0200

    Ignore untyped libraries

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+from dash import Dash, html, dcc, callback, Output, Input  # type: ignore
+import plotly.express as px  # type: ignore
 import requests
 from io import StringIO
 import csv
 from natsort import natsorted
 import pandas as pd
-import dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc  # type: ignore
 from typing import Optional
 
 
@@ -147,7 +147,6 @@ def update_graph(
     f = StringIO(res)
     reader = csv.reader(f, delimiter=",")
     rows: list[list] = [row for row in reader]
-    print(rows)
     full_df = pd.DataFrame(rows[1:], columns=rows[0])
 
     possible_colors = ["technology", "carrier"]
