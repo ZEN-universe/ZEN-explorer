2bf834dbe3532f69e9e19ace1e651b7a89c0b856
Vinzenz <muserv@ethz.ch>
2023-11-14 18:14:22 +0100

    Added the energy balance to the plots

--- pages/balance.py
+++ pages/balance.py
@@ -10,6 +10,7 @@ from time import perf_counter
 import dash  # type: ignore
 from dash.exceptions import PreventUpdate  # type: ignore
 from config import config
+import plotly.graph_objects as go  # type: ignore
 
 
 server_url = config.TEMPLE_URL
@@ -74,7 +75,6 @@ layout = dbc.Container(
                         dcc.Dropdown(
                             [],
                             id="balance-scenario-selection",
-                            value="scenario_",
                             clearable=False,
                         ),
                     ],
@@ -83,11 +83,10 @@ layout = dbc.Container(
                 html.Div(
                     [
                         dbc.Label(
-                            "Component",
+                            "Carrier",
                         ),
                         dcc.Dropdown(
-                            ["cost_carrier"],
-                            "cost_carrier",
+                            [],
                             id="balance-component-selection",
                             clearable=False,
                             disabled=True,
@@ -98,7 +97,14 @@ layout = dbc.Container(
                 html.Div(
                     [
                         dbc.Label("Node"),
-                        dcc.Dropdown([], value=sum_string, id="balance-node-selection"),
+                        dcc.Dropdown([], id="balance-node-selection"),
+                    ],
+                    className="mt-3",
+                ),
+                html.Div(
+                    [
+                        dbc.Label("Year"),
+                        dcc.Dropdown([], id="balance-year-selection"),
                     ],
                     className="mt-3",
                 ),
@@ -107,7 +113,6 @@ layout = dbc.Container(
         html.Hr(),
         dbc.Row(
             [
-                dbc.Col(filters, md=3),
                 dbc.Col(
                     dcc.Loading(
                         id="loading-1",
@@ -120,6 +125,7 @@ layout = dbc.Container(
         dcc.Store(id="balance-solution-list"),
         dcc.Store(id="balance-available-components"),
         dcc.Store(id="balance-component-details"),
+        dcc.Store(id="balance-current-solution"),
     ]
 )
 
@@ -144,6 +150,14 @@ def test(a):
         Output("balance-scenario-selection", "disabled"),
         Output("balance-component-selection", "value"),
         Output("balance-scenario-selection", "value"),
+        Output("balance-component-selection", "options"),
+        Output("balance-component-selection", "disabled"),
+        Output("balance-node-selection", "options"),
+        Output("balance-node-selection", "disabled"),
+        Output("balance-node-selection", "value"),
+        Output("balance-current-solution", "data"),
+        Output("balance-year-selection", "options"),
+        Output("balance-year-selection", "value"),
     ],
     [
         Input("balance-solution-list", "data"),
@@ -157,103 +171,21 @@ def activate_solution(solutions: dict[Any, Any], solution: str):
     index = [i["name"] for i in solutions].index(solution)
     current_soultion = solutions[index]
     scenarios = natsorted(list(current_soultion["scenarios"]))
-    return scenarios, len(scenarios) == 1, None, scenarios[0]
-
-
-@callback(
-    Output("balance-available-components", "data"),
-    [
-        Input("balance-solution-selection", "value"),
-        Input("balance-scenario-selection", "value"),
-    ],
-)  # type: ignore
-def activate_components(solution_name: str, scenario_name: Optional[str]):
-    if solution_name is None:
-        raise PreventUpdate
-
-    if scenario_name is None:
-        scenario_name = "scenario_"
-
-    data = requests.get(
-        server_url + f"solutions/{solution_name}/components"
+    years = list(range(current_soultion["optimized_years"]))
+    return (
+        scenarios,
+        len(scenarios) == 1,
+        None,
+        scenarios[0] if len(scenarios) > 1 else None,
+        current_soultion["carriers"],
+        len(current_soultion["carriers"]) == 0,
+        current_soultion["nodes"],
+        len(current_soultion["nodes"]) == 0,
+        None,
+        current_soultion,
+        years,
+        None,
     )
-
-    return data.json()
-
-
-@callback(
-    [
-        Output("balance-component-selection", "options"),
-        Output("balance-component-selection", "disabled"),
-    ],
-    Input("balance-available-components", "data"),
-)  # type: ignore
-def update_components_options(available_components):
-    components = [i["component_name"] for i in available_components if not i["yearly"]]
-    return components, len(components) == 0
-
-
-@callback(
-    [
-        Output("balance-component-details", "data"),
-        Output("balance-technology-selection", "value"),
-        Output("balance-carrier-selection", "value"),
-        Output("balance-node-selection", "value"),
-    ],
-    [
-        Input("balance-component-selection", "value"),
-        Input("balance-available-components", "data"),
-    ],
-)  # type: ignore
-def update_active_component(component: str, available_components: dict[Any, Any]):
-    if available_components is None:
-        raise PreventUpdate
-
-    component_names = [i["component_name"] for i in available_components]
-
-    if component not in component_names:
-        return {}, None, None, None
-
-    outputs = [available_components[component_names.index(component)]] + [
-        None,
-        None,
-        sum_string,
-    ]
-    return tuple(outputs)
-
-
-@callback(
-    [
-        Output("balance-technology-selection", "options"),
-        Output("balance-carrier-selection", "options"),
-        Output("balance-node-selection", "options"),
-        Output("balance-technology-selection", "disabled"),
-        Output("balance-carrier-selection", "disabled"),
-        Output("balance-node-selection", "disabled"),
-    ],
-    Input("balance-component-details", "data"),
-)  # type: ignore
-def update_options(component_detail: dict[Any, Any]):
-    option_tuples: list[tuple[str, list[str]]] = [
-        ("technology", []),
-        ("carrier", []),
-        ("node", [sum_string]),
-    ]
-    if component_detail is None:
-        return 3 * [[]] + 3 * [True]
-    if "levels" not in component_detail:
-        return [[]] * len(option_tuples) + [True] * len(option_tuples)
-
-    level_titles = [i["title"] for i in component_detail["levels"]]
-
-    for option_tuple in option_tuples:
-        if option_tuple[0] in level_titles:
-            index = level_titles.index(option_tuple[0])
-            for i in component_detail["levels"][index]["names"]:
-                option_tuple[1].append(i)
-
-    ans = [i[1] for i in option_tuples] + [len(i[1]) == 0 for i in option_tuples]
-    return tuple(ans)
 
 
 @callback(
@@ -261,61 +193,39 @@ def update_options(component_detail: dict[Any, Any]):
     [
         Input("balance-solution-selection", "value"),
         Input("balance-component-selection", "value"),
-        Input("balance-technology-selection", "value"),
-        Input("balance-carrier-selection", "value"),
         Input("balance-scenario-selection", "value"),
         Input("balance-node-selection", "value"),
-        Input("balance-component-details", "data"),
+        Input("balance-year-selection", "value"),
+        Input("balance-current-solution", "data"),
     ],
 )  # type: ignore
 def update_graph(
     solution: Optional[str],
-    component: Optional[str],
-    technology: Optional[str],
     carrier: Optional[str],
     scenario: Optional[str],
     node: Optional[str],
-    details: Optional[dict[str, Any]],
+    year: Optional[int],
+    current_solution: dict[str, Any],
 ):
     if solution is None:
         return get_empty_plot("Select a solution")
 
-    if scenario is None:
-        return get_empty_plot("Select a Scenario")
+    if carrier is None:
+        return get_empty_plot("Select a Carrier")
 
-    if component is None:
-        return get_empty_plot("Select a Component")
+    if node is None:
+        return get_empty_plot("Select a Node")
 
-    if details is None:
-        return get_empty_plot("No data found.")
-
-    index_sets = []
-
-    level_tuples = [("technology", technology), ("carrier", carrier), ("node", node)]
-
-    for i in level_tuples:
-        if i[1] is not None:
-            current: dict[str, list[str] | str] = {
-                "behaviour": "sum",
-                "index_title": i[0],
-            }
-            if i[1] != sum_string:
-                current["indices"] = [i[1]]
-
-            index_sets.append(current)
-
-    request = {
-        "aggregate_years": False,
-        "scenario": scenario,
-        "solution_name": solution,
-        "component": component,
-        "data_request": {"default": "series", "index_sets": index_sets},
-    }
+    if year is None:
+        return get_empty_plot("Select a year")
 
     start = perf_counter()
-
     try:
-        result = requests.post(f"{server_url}solutions/get_data", json=request)
+        url = f"{server_url}solutions/get_energy_balance/{solution}/{node}/{carrier}/?year={year}"
+        if scenario is not None:
+            url += f"scenario={scenario}"
+        result = requests.get(url)
+
     except Exception:
         return get_empty_plot("Cannot process request")
 
@@ -330,26 +240,21 @@ def update_graph(
 
     if len(full_df) <= 1:
         return get_empty_plot("No data to plot.")
+    full_df = pd.melt(full_df, id_vars=["variable", "technology"], var_name="time_step")
 
-    possible_levels: list[str] = [
-        i for i in ["technology", "node", "carrier"] if i in full_df.columns
-    ]
-
-    possible_plot_dimensions: list[str] = ["color", "pattern_shape"]
-
-    kwargs: dict[str, str] = {}
-
-    for k, keyword in enumerate(possible_plot_dimensions):
-        try:
-            kwargs[keyword] = possible_levels[k]
-        except IndexError:
-            break
-
-    time_name = "year" if "year" in full_df.columns else "time_step"
-
-    fig = px.area(full_df, x=time_name, y="value", orientation="v", **kwargs)
-
-    n_timesteps_per_year = details["aggregated_time_steps_per_year"]
+    non_demand_df = full_df[full_df["variable"] != "demand"]
+    fig = px.area(
+        non_demand_df, x="time_step", y="value", orientation="v", color="technology"
+    )
+    demand_df = full_df[full_df["variable"] == "demand"]
+    trace1 = go.Scatter(
+        x=demand_df["time_step"],
+        y=demand_df["value"],
+        name="demand",
+        marker=dict(color="black"),
+    )
+    fig.add_trace(trace1)
+    n_timesteps_per_year = current_solution["total_hours_per_year"]
     tickvals = [i for i in range(len(full_df) - 1) if i % n_timesteps_per_year == 0]
 
     fig.update_xaxes(
