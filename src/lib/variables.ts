b2512171b419e60a6b7d79e17b7a39194c42c760
Jacob Mannhardt <97548957+jacob-mannhardt@users.noreply.github.com>
2024-11-20 16:34:25 +0100

    bug fix so that smaller_than correctly returns that 1.9.0 < 2.0.0

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -7,9 +7,9 @@ interface Versions {
     [key: string]: NameMap
 }
 
-
+// sort so that a potential version 2.x.x has precedence over 1.9.0
 const versions: Versions = {
-    "1.9.0": {
+    "1.9.0": { 
         capex_yearly: "cost_capex_yearly",
         opex_yearly: "cost_opex_yearly",
         cost_opex_total: "cost_opex_yearly_total",
@@ -17,7 +17,7 @@ const versions: Versions = {
         cost_capex_total: "cost_capex_yearly_total",
         cost_capex: "cost_capex_overnight"
     }
-}
+} 
 
 function smaller_than(version_a: string, version_b: string) {
     let split_a = version_a.split(".")
@@ -25,11 +25,13 @@ function smaller_than(version_a: string, version_b: string) {
 
     for (let i = 0; i < 3; i++) {
 
-        if (Number(split_a[i]) > Number(split_b[i])) {
+        if (Number(split_a[i]) < Number(split_b[i])) {
+            return true
+        } else if (Number(split_a[i]) > Number(split_b[i])) {
             return false
         }
     }
-    return true
+    return false
 }
 
 export function get_variable_name(name: string, version: string = "0.0.0") {
