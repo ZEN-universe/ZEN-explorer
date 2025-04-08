82b4fcffa17971510f110e28d7c162b788f678ca
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2025-04-08 09:09:03 +0200

    Add tooltips.

--- src/components/MapPlot.svelte
+++ src/components/MapPlot.svelte
@@ -2,15 +2,17 @@
 	import { pie, arc } from 'd3-shape';
 	import { scaleOrdinal } from 'd3-scale';
 	import { geoMercator, geoPath } from 'd3-geo';
-	import { csv } from 'd3-fetch';
 	import { schemeCategory10 } from 'd3-scale-chromatic';
-	import { onMount } from 'svelte';
+	import { pointer } from 'd3-selection';
 	import { feature, mesh } from 'topojson-client';
 	import countries10m from '../geojson/world.json'; // TODO: use a better world map
 	// import countries10m from 'world-atlas/countries-10m.json';
 	import { _detectPlatform } from 'chart.js';
-	import type { DSVRowArray, DSVRowString } from 'd3';
-	import type { GeometryCollection, Topology } from 'topojson-specification';
+	import { polygonContains, type DSVRowArray, type DSVRowString } from 'd3';
+	import type { MultiPolygon, Polygon, Position } from 'geojson';
+	import type { GeometryCollection, GeometryObject, Topology } from 'topojson-specification';
+	import { onDestroy, onMount } from 'svelte';
+	import { get } from 'svelte/store';
 
 	export interface DataRow {
 		technology: string;
@@ -63,10 +65,35 @@
 	}
 	let { data, edges = {} }: Props = $props();
 
+	interface Pie {
+		lon: number;
+		lat: number;
+		label: string;
+		country: number;
+		total: number;
+		data: {
+			color: string;
+			arc: string | null;
+			technology: string;
+			value: number;
+		}[];
+	}
+
+	let svg: SVGSVGElement;
+	let tooltip: HTMLDivElement;
+
 	let width = $state(936);
 	let height = $state(500);
 	let maxRadius = $state(30);
 	let maxEdgeWidth = $state(8);
+
+	let tooltipY = $state(0);
+	let tooltipYOffset = $state(0);
+	let tooltipX = $state(0);
+	let tooltipHeight = $derived(() => tooltip?.clientHeight || 0);
+	let tooltipOnTop = $derived(tooltipY - tooltipYOffset > tooltipHeight());
+
+	let selected_pie: Pie | null = $state(null);
 
 	let _projection = $derived(
 		geoMercator()
@@ -94,39 +121,40 @@
 		}, 0)
 	);
 
-	let land: string | null = $derived(
-		_path(
-			feature(
-				countries10m as unknown as Topology,
-				countries10m.objects.countries as GeometryCollection
-			)
+	let countries = $derived(
+		feature(
+			countries10m as unknown as Topology,
+			countries10m.objects.countries as GeometryCollection
 		)
 	);
+	let land: string | null = $derived(_path(countries));
 	let meshes: string | null = $derived(
 		_path(
 			mesh(
 				countries10m as unknown as Topology,
-				countries10m.objects.countries as GeometryCollection
+				countries10m.objects.countries as GeometryCollection,
+				(a: GeometryObject, b: GeometryObject) => a !== b
 			)
 		)
 	);
-	let pies = $derived(
+	let pies: Pie[] = $derived(
 		Object.entries(data).map(([node, values]: [string, DataRow[]]) => {
 			const total = values.reduce((acc: number, row: DataRow) => acc + row.value, 0);
-			const _arc = arc()
-				.innerRadius(0)
-				.outerRadius((maxRadius * total) / maxTotal);
+			const _arc = arc().innerRadius(0).outerRadius(computeRadius(total));
 			const [lon, lat] = nodeCoords[node] || [40, 40];
 
 			return {
-				lat: lat,
 				lon: lon,
+				lat: lat,
 				label: node,
+				country: Number(getCountry([lon, lat])?.id || -1),
 				total: total,
 				data: _pie(values.map((d) => d.value)).map((p: any, i: number) => {
 					return {
 						color: _color(i.toString()),
-						arc: _arc(p)
+						arc: _arc(p),
+						technology: values[i].technology,
+						value: p.data
 					};
 				})
 			};
@@ -144,41 +172,143 @@
 		})
 	);
 
+	onMount(() => {
+		window.addEventListener('resize', onResize);
+		onResize();
+	});
+
+	onDestroy(() => {
+		window.removeEventListener('resize', onResize);
+	});
+
 	function straightLine(start: [number, number], end: [number, number]) {
 		const [x1, y1] = _projection(start)!;
 		const [x2, y2] = _projection(end)!;
 		return `M${x1},${y1} L${x2},${y2}`;
 	}
+
+	function getCountry(coord: [number, number]) {
+		return countries.features.find((f) => {
+			return (f.geometry as Polygon | MultiPolygon).coordinates.find((c1) => {
+				return (
+					polygonContains(c1 as [number, number][], coord) ||
+					c1.find((c2) => polygonContains(c2 as [number, number][], coord))
+				);
+			});
+		});
+	}
+
+	function computeRadius(total: number) {
+		return (total / maxTotal) * maxRadius;
+	}
+
+	function onResize() {
+		const { width: w, height: h } = svg.parentElement!.getBoundingClientRect();
+		width = w;
+		height = h;
+	}
+
+	function onMouseMove(event: MouseEvent) {
+		const pos = _projection.invert!(pointer(event));
+		if (!pos) return;
+
+		const country = getCountry(pos);
+		if (!country) return;
+
+		const pie = pies.find((p) => p.country == country?.id);
+		selected_pie = pie || null;
+
+		if (pie) {
+			const [x, y] = _projection([pie.lon, pie.lat])!;
+			tooltipY = y;
+			tooltipX = x;
+			tooltipYOffset = computeRadius(pie.total) + 5;
+		}
+	}
+
+	function stringify(value: string) {
+		let res = value.replace(/_/g, ' ');
+		return res.charAt(0).toUpperCase() + res.slice(1);
+	}
 </script>
 
-<svg {width} {height}>
-	<g>
-		<path class="land" d={land} />
-		<path class="mesh" d={meshes} fill="none" stroke="white" stroke-width="1px" />
-	</g>
-	<g>
-		{#each lines as line}
-			<g class="line">
-				<path d={straightLine(line.start, line.end)} stroke="#666" stroke-width={line.width} />
-			</g>
-		{/each}
-	</g>
-	<g>
-		{#each pies as pie}
-			<g class="pies" transform={`translate(${_projection([pie.lon, pie.lat])})`}>
-				{#each pie.data as d}
-					<path class="arc" d={d.arc} fill={d.color} />
-				{/each}
-			</g>
-		{/each}
-	</g>
-</svg>
+<div class="position-relative">
+	<!-- svelte-ignore a11y_no_static_element_interactions -->
+	<svg class="map" {width} {height} bind:this={svg} onmousemove={onMouseMove}>
+		<g>
+			<path class="land" d={land} />
+			<path class="mesh" d={meshes} fill="none" stroke="white" stroke-width="1px" />
+		</g>
+		<g>
+			{#each lines as line}
+				<g class="line">
+					<path d={straightLine(line.start, line.end)} stroke="#666" stroke-width={line.width} />
+				</g>
+			{/each}
+		</g>
+		<g>
+			{#each pies as pie}
+				<g class="pies" transform={`translate(${_projection([pie.lon, pie.lat])})`}>
+					{#each pie.data as d}
+						<path class="arc" d={d.arc} fill={d.color} />
+					{/each}
+				</g>
+			{/each}
+		</g>
+	</svg>
+	<div
+		class="position-absolute bg-black bg-opacity-75 text-white p-2 rounded fs-8 pe-none"
+		style:top={tooltipOnTop ? `${tooltipY - tooltipYOffset}px` : `${tooltipY + tooltipYOffset}px`}
+		style:left={`${tooltipX}px`}
+		style:transform={tooltipOnTop ? `translate(-50%, -100%)` : `translate(-50%, 0%)`}
+		style:opacity={selected_pie ? 1 : 0}
+		style:transition="opacity 0.3s"
+		bind:this={tooltip}
+	>
+		{#if tooltipOnTop}
+			<svg
+				class="position-absolute translate-middle-x opacity-75 start-50"
+				style:bottom="-5px"
+				width="10"
+				height="5"
+			>
+				<polygon points="0,0 10,0 5,5" fill="black" />
+			</svg>
+		{:else}
+			<svg
+				class="position-absolute translate-middle-x opacity-75 start-50"
+				style:top="-5px"
+				width="10"
+				height="5"
+			>
+				<polygon points="0,5 10,5 5,0" fill="black" />
+			</svg>
+		{/if}
+		{#if selected_pie}
+			<h5 class="fs-7 mb-0">{selected_pie.label}</h5>
+			{#each selected_pie.data as d}
+				<div>
+					<svg width="16" height="16">
+						<rect width="16" height="16" fill={d.color} />
+					</svg>
+					{stringify(d.technology)}: {d.value.toFixed(3)}
+				</div>
+			{/each}
+		{/if}
+	</div>
+</div>
 
 <style>
-	svg {
+	.map {
 		background: #91a9cf;
 	}
 	.land {
 		fill: #b3c497;
+	}
+	.fs-7 {
+		font-size: 0.875rem;
+	}
+	.fs-8 {
+		font-size: 0.75rem;
 	}
 </style>
