b2b72ab0e96179bd274ce5a618a209f5bbe1d565
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2025-07-08 11:20:27 +0200

    [Capacity] Write test for each form element on the page.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+	projects: [
+		{
+			name: 'chromium',
+			use: { browserName: 'chromium' }
+		}
+	],
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173
