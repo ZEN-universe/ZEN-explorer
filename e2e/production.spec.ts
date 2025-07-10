f065db1ff9ed06e4b8d58afec9fda41944558780
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2025-07-10 16:09:04 +0200

    [Production] Write tests for each form element.

---
+++ e2e/production.spec.ts
@@ -0,0 +1,80 @@
+import { test, expect } from '@playwright/test';
+
+test(`production: carrier`, async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('carbon');
+	await expect(page.locator('#chart')).toHaveScreenshot(`eeht_pf/carbon.png`);
+});
+
+test('production: conversion subdivision', async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('biomass');
+  await page.locator('#subdivision0').check();
+	await expect(page.locator('#chart')).toHaveScreenshot(
+		`eeht_pf/biomass/conversion-subdivision.png`
+	);
+});
+
+test('production: conversion technology', async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('biomass');
+  await page.getByRole('checkbox', { name: 'biomass_boiler', exact: true }).uncheck();
+	await expect(page.locator('#chart')).toHaveScreenshot(
+		`eeht_pf/biomass/conversion-technology-without-biomass_boiler.png`
+	);
+});
+
+test('production: normalisation', async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('biomass');
+  await page.getByRole('switch', { name: 'Normalization off' }).click();
+	await expect(page.locator('#chart')).toHaveScreenshot(
+		`eeht_pf/biomass/normalization-on.png`
+	);
+});
+
+test('production: nodes', async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('biomass');
+  await page.locator('div').filter({ hasText: /^Nodes Deselect all$/ }).getByRole('button').click();
+  await expect(page.locator('.plot')).toHaveText('No data with this selection.');
+  await page.getByRole('checkbox', { name: 'DE' }).check();
+	await expect(page.locator('#chart')).toHaveScreenshot(
+		`eeht_pf/biomass/nodes-only-DE.png`
+	);
+});
+
+test('production: years', async ({ page }) => {
+	await page.goto('/transition/production/');
+	await page
+		.getByLabel('Solution', { exact: true })
+		.selectOption('european_electricity_heating_transition');
+	await page.getByLabel('Subsolution').selectOption('perfect_foresight');
+	await page.getByLabel('Carrier').selectOption('biomass');
+  await page.locator('div').filter({ hasText: /^Years Deselect all$/ }).getByRole('button').click();
+  await expect(page.locator('.plot')).toHaveText('No data with this selection.');
+  await page.getByRole('checkbox', { name: '2024' }).check();
+	await expect(page.locator('#chart')).toHaveScreenshot(
+		`eeht_pf/biomass/years-only-2024.png`
+	);
+});
