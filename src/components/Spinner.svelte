6307c11aac119a22ccbad2496713e28966fb9e85
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2025-12-30 11:11:15 +0100

    Adjust windmill path.

--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -1,6 +1,6 @@
 <div class="flex flex-col items-center mt-8 md:mt-30" role="status">
 	<svg class="h-40 w-40" version="1.1" viewBox="-1.5 -0.8 12.0 10.6">
-		<path d="m3.28 10.671 0.51941-5.696 0.85573 5.7374z" class="fill-gray-500 dark:fill-gray-400" />
+		<path d="M3.28 10.671 3.79941,4.975 4.318,10.671 Z" class="fill-gray-500 dark:fill-gray-400" />
 		<circle cx="3.78" cy="4.215" r=".42995" class="fill-gray-500 dark:fill-gray-400" />
 		<g
 			class="animate-[spin_2s_linear_infinite] fill-none stroke-gray-500 dark:stroke-gray-400"
