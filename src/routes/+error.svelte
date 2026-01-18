e7c2e6584001e5fafea209650ee57d64ebd542b0
Manuel Dublanc <19774382+manud99@users.noreply.github.com>
2026-01-18 14:55:01 +0100

    Add error route.

---
+++ src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import ErrorMessage from "$components/ErrorMessage.svelte";
+	import { page } from '$app/state';
+</script>
+
+<ErrorMessage message={`${page.status} - ${page.error?.message}`}></ErrorMessage>
